a293e20dfb0a10fc8200782df65d905bdc7d4731
Giulio Turrisi <giulioturrisi@hotmail.it>
2026-03-06 10:54:00 +0100

    Update ROS_DISCOVERY_SERVER with multiple addresses

--- deploy/dls2_connect.sh
+++ deploy/dls2_connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Remeber to run this file with: source dls2_connect.sh"
-export ROS_DISCOVERY_SERVER=127.0.0.1:11814 && 
+export ROS_DISCOVERY_SERVER="127.0.0.1:11812;127.0.0.1:11813;127.0.0.1:11814;127.0.0.1:11818" && 
 export ROS_SUPER_CLIENT=TRUE && 
 export ROS_LOCALHOST_ONLY=0 &&
 ros2 daemon stop &&
